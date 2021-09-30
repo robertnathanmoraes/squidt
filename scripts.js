46e649b5ce9a28334369b2eba7bd32c0664b6ac7
rober <robert.nunes@evo1.com.br>
2021-09-29 23:33:12 -0300

    first

--- scripts.js
+++ scripts.js
@@ -1,3 +1,25 @@
-document.addEventListener("DOMContentLoaded", function() {
-  // Código que será executado quando o navegador carregar
+document.addEventListener("DOMContentLoaded", function () {
+        const url = 'https://us-central1-squid-apis.cloudfunctions.net/test-front-basic'
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                let usuario = data;
+                let tabela = document.getElementById("tabela")
+                data.forEach(element => {
+                    let linha = crialinha(element);
+                    tabela.appendChild(linha)
+                })
+            })
 });
+
+function crialinha(usuario){
+    console.log(usuario)
+    linha = document.createElement("div")
+    tdimage = document.createElement("img")
+    tdimage.src = usuario.imagens['resolucaoPadrao'].url
+    tdimage.innerHTML = tdimage.src
+
+    linha.appendChild(tdimage)
+
+    return linha
+}
