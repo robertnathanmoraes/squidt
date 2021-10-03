9d11d155c2c5c099150b299631b3c35271637074
rober <robert.nunes@evo1.com.br>
2021-10-03 09:47:16 -0300

    hover

--- scripts.js
+++ scripts.js
@@ -1,32 +1,36 @@
 document.addEventListener("DOMContentLoaded", function () {
+
     const url = 'https://us-central1-squid-apis.cloudfunctions.net/test-front-basic'
+
     fetch(url)
         .then(response => response.json())
         .then(data => {
+
             let usuario = data;
             let tabela = document.getElementById("tabela")
+
             data.forEach(element => {
-                let linha = crialinha(element);
-                tabela.appendChild(linha)
+                let item = create(element);
+                tabela.appendChild(item)
             })
+
         })
 });
 
-function crialinha(usuario) {
-    console.log(usuario)
+function create(items) {
+
     a = document.createElement("a")
     linha = document.createElement("div")
-    tdimage = document.createElement("img")
+    image = document.createElement("img")
 
     texto = document.createElement("tbody")
-    icon1 = document.createElement("i")
-    icon2 = document.createElement("i")
-    icon3 = document.createElement("i")
-    icon4 = document.createElement("i")
-    tdimage.src = usuario.imagens['resolucaoPadrao'].url
+    iconLike = document.createElement("i")
+    iconComment = document.createElement("i")
+    iconCalendar = document.createElement("i")
+    image.src = items.imagens['resolucaoPadrao'].url
 
     //imagem class
-    tdimage.classList.add('col-xs-12', 'img-post')
+    image.classList.add('col-xs-12', 'img-post')
 
     //div class
     linha.classList.add('margin-right', 'hover')
@@ -35,29 +39,29 @@ function crialinha(usuario) {
     texto.classList.add('text-style')
 
     //icon class
-    icon2.classList.add('fa', 'fa-heart', 'icon')
-    icon4.classList.add('fa', 'fa-comment', 'icon')
-    icon3.classList.add('fa', 'fa-calendar', 'icon')
+    iconLike.classList.add('fa', 'fa-heart', 'icon')
+    iconComment.classList.add('fa', 'fa-comment', 'icon')
+    iconCalendar.classList.add('fa', 'fa-calendar', 'icon')
 
     let nick = texto.insertRow()
     let like = texto.insertRow()
     let qtdComents = texto.insertRow()
     let dataHora = texto.insertRow()
 
-    let date = new Date(usuario['criadoEm'])
+    let date = new Date(items['criadoEm'])
 
-    a.setAttribute('href', usuario['link'])
+    a.setAttribute('href', items['link'])
     a.setAttribute('target', '_blank')
 
-    like.textContent = usuario['upvotes']
-    nick.textContent = '@' + usuario['usuario'].username
-    qtdComents.textContent = usuario['comentarios']
+    like.textContent = items['upvotes']
+    nick.textContent = '@' + items['usuario'].username
+    qtdComents.textContent = items['comentarios']
     dataHora.textContent = date.getDay() + '/' + date.getMonth() + '/' + date.getFullYear() + ' ' + date.getHours() + ':' + ('0' + date.getMinutes()).slice(-2)
-    like.appendChild(icon2)
-    dataHora.appendChild(icon3)
-    qtdComents.appendChild(icon4)
+    like.appendChild(iconLike)
+    dataHora.appendChild(iconCalendar)
+    qtdComents.appendChild(iconComment)
 
-    linha.appendChild(tdimage)
+    linha.appendChild(image)
     linha.appendChild(texto)
     a.appendChild(linha)
 
